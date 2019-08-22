3f2907ee1d3ebfede1302bf2d2ac68656d6a7dd8
xanvranck <vranckaert.xan@gmail.com>
2019-08-22 19:05:50 +0200

    added counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {  CountDown} from "ng2-date-countdown";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountDown
   ],
   imports: [
     BrowserModule,
