f1a5571e598ea8be44b0e3b3a150a9a92fb1108d
xanvranck <vranckaert.xan@gmail.com>
2019-09-29 11:45:30 +0200

    added more tips and counters

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -15,22 +15,38 @@ export class CountdownComponent {
   interval: any;
   now: Date;
   countdownDate: Date;
+  thirdTipDate: Date;
+  fourthTipDate: Date;
+  fifthTipDate: Date;
+  sixthTipDate: Date;
+  seventhTipDate: Date;
 
   constructor() {
-   
     this.interval = interval(1000);
+    this.thirdTipDate = new Date(2019, 9, 13, 18, 0, 0, 0)
+    this.fourthTipDate = new Date(2019, 9, 15, 18, 0, 0, 0)
+    this.fifthTipDate = new Date(2019, 9, 18, 9, 0, 0, 0)
+    this.sixthTipDate = new Date(2019, 10, 14, 18, 0, 0, 0)
+    this.seventhTipDate = new Date(2019, 11, 19, 18, 0, 0, 0)
     setInterval(() => {this.countdown()}, 1000)
   }
 
   countdown() {
-    this.countdownDate = new Date(2019, 8, 8, 18,0,0,0);
+    this.countdownDate = this.thirdTipDate;
     this.now = new Date();
-    if(this.now > this.countdownDate) {
-      this.countdownDate = new Date(2019, 8, 9, 18,0,0,0)
+    if(this.now > this.thirdTipDate && this.now < this.fourthTipDate) {
+      this.countdownDate = this.fourthTipDate
     }
-    if(this.now > this.countdownDate) {
-      this.countdownDate = new Date(2019, 9, 13, 18,0,0,0)
+    if(this.now > this.fourthTipDate && this.now < this.fifthTipDate) {
+      this.countdownDate = this.fifthTipDate
     }
+    if(this.now > this.fifthTipDate && this.now < this.sixthTipDate) {
+      this.countdownDate = this.sixthTipDate
+    }
+    if(this.now > this.sixthTipDate && this.now < this.seventhTipDate) {
+      this.countdownDate = this.seventhTipDate
+    }
+
     // Find the distance between now and the count down date
     var distance = this.countdownDate.getTime() - this.now.getTime();
     // Time calculations for days, hours, minutes and seconds
--- src/app/tip/tip.component.ts
+++ src/app/tip/tip.component.ts
@@ -7,24 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TipComponent implements OnInit {
 
-  now:Date;
+  now: Date;
   firstTipDate: Date;
   secondTipDate: Date;
-  
-  constructor() { 
-    this.firstTipDate = new Date(2019, 8, 8, 18,0,0,0);
-    this.secondTipDate = new Date(2019, 8, 9, 18,0,0,0);
+  thirdTipDate: Date;
+  fourthTipDate: Date;
+  fifthTipDate: Date;
+  sixthTipDate: Date;
+  seventhTipDate: Date;
+
+  constructor() {
+    this.firstTipDate = new Date(2019, 8, 8, 18, 0, 0, 0);
+    this.secondTipDate = new Date(2019, 8, 9, 18, 0, 0, 0);
+    this.thirdTipDate = new Date(2019, 9, 13, 18, 0, 0, 0)
+    this.fourthTipDate = new Date(2019, 9, 15, 18, 0, 0, 0)
+    this.fifthTipDate = new Date(2019, 9, 18, 9, 0, 0, 0)
+    this.sixthTipDate = new Date(2019, 10, 14, 18, 0, 0, 0)
+    this.seventhTipDate = new Date(2019, 11, 19, 18, 0, 0, 0)
     this.now = new Date();
   }
 
   ngOnInit() {
   }
 
-  beforeFirstTip():boolean {
+  beforeFirstTip(): boolean {
     return this.now < this.firstTipDate;
   }
 
-  secondTip():boolean {
+  secondTip(): boolean {
     return this.now > this.secondTipDate;
+  }
+
+  thirdTip(): boolean {
+    return this.now > this.thirdTipDate;
+  }
+
+  fourthTip(): boolean {
+    return this.now > this.fourthTipDate;
+  }
+
+  fifthTip(): boolean {
+    return this.now > this.fifthTipDate;
+  }
+
+  sixthTip(): boolean {
+    return this.now > this.sixthTipDate;
+  }
+
+  seventhTip(): boolean {
+    return this.now > this.seventhTipDate;
   }
 }
